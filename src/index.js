394fe9d8105df85d9fa6eb9d7964f8fc1427b5a5
dukemuge <dukemuge232@gmail.com>
2021-11-07 04:45:39 -0500

    Add avatar component  and its styling

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function Avatar() {
+    return (
+    <img
+    src="https://www.gravatar.com/avatar/nothing"
+    className="avatar"
+    alt="avatar" />
+    );
+    }
+
 function Tweet() {
     return (
-
-        <
-        div className = "tweet" >
-        Tweet
-
-        <
-        /div>
+    <div className="tweet">
+    <Avatar/>
+    Tweet
+    </div>
     );
-}
+    }
 
-ReactDOM.render( < Tweet / > ,
+
+
+
+ReactDOM.render( < Tweet /> ,
     document.querySelector('#root'));
